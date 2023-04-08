cea812d295fb1d88cd3ddcb35dd7e0a754b60de1
kshitij <kshitij_987@protonmail.com>
2023-04-09 00:35:06 +0530

    added global navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,7 +1,19 @@
+import Logo from "../Logo";
+import Container from "../Container";
+import Search from "../Search";
+import UserMenu from '../UserMenu';
 export default function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
+        </Container>
+      </div>
     </div>
-  )
+  );
 }
