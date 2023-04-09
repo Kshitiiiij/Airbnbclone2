d957eece1a4280b2aa9cce6fa81e46320773a8f2
kshitij <kshitij_987@protonmail.com>
2023-04-10 02:20:29 +0530

    register modal

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,12 @@
+"use client";
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FaGoogle } from "react-icons/fa";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+const RegisterModal = () => {
+  return <div className=""></div>;
+};
+
+export default RegisterModal;
