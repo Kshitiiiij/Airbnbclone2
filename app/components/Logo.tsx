8fb2788ad846a5ca07b90fd30aa46ab965ec4dc8
kshitij <kshitij_987@protonmail.com>
2023-04-12 22:08:20 +0530

    added home route to arbnb logo

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import Image from "next/image"
-import {useRouter} from "next/router"
+import {useRouter} from "next/navigation"
 
 const Logo = () => {
+    const router = useRouter()
     return (
+        
         <Image
+        onClick={() => (router.push('/'))}
         alt="Air BnB Logo"
         className="hidden md:block cursor-pointer"
         height={70}
