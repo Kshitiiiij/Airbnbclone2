6eae5e190c6e4f94f5e275ddb3ce37d3d2ae9ed1
kshitij <kshitij_987@protonmail.com>
2023-04-15 00:55:51 +0530

    Added state for when no listing is presentd

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-
+import Container from "./components/Container";
+import ClientOnly from "./components/ClientOnly";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly >
+        <EmptyState 
+        showReset
+        />
+      </ClientOnly>
+    );
+  }
+
   return (
-   <div className=""></div>
-  )
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-4 2xl:grid-cols-6 gap-8">
+          <div className="">My Listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
