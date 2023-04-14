bd23f044afd7503eba9133f3b004372714f66cb2
kshitij <kshitij_987@protonmail.com>
2023-04-15 04:00:24 +0530

    added listings

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import Container from "./components/Container";
 import ClientOnly from "./components/ClientOnly";
 import EmptyState from "./components/EmptyState";
+import getCurrentListings from "./actions/getCurrentListings";
+import ListingCard from "./components/listing/ListingCard";
+import { getCurrentUser } from "./actions/getCurrentuser";
 
-export default function Home() {
+export default async function Home() {
   const isEmpty = true;
+  const listings = await getCurrentListings();
+  const currentUser = await getCurrentUser();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return (
-      <ClientOnly >
-        <EmptyState 
-        showReset
-        />
+      <ClientOnly>
+        <EmptyState showReset />
       </ClientOnly>
     );
   }
@@ -19,7 +22,15 @@ export default function Home() {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-4 2xl:grid-cols-6 gap-8">
-          <div className="">My Listings</div>
+          {listings.map((listing) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
         </div>
       </Container>
     </ClientOnly>
